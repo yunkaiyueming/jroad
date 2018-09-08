b1f0e84aa2fae6468fd03fad72f29321d8b4ecbf
yunkai <819664855@qq.com>
2018-09-08 15:53:26 +0800

    java

---
+++ cron/CronStudy.java
@@ -0,0 +1,36 @@
+package cron;
+
+public class CronStudy implements Runnable {
+	
+	public static void main(String[] args){
+		CronStudy cs = new CronStudy();
+		cs.go();
+		//cs.go1();
+	}
+	
+	public void run(){
+		while(true){
+			System.out.println("start study.....");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	public void go() {
+		Thread t = new Thread(this);
+		t.start();//异步调用本对象的run方法
+		
+		System.out.println("run end...");
+	}
+	
+	public void go1() {
+		this.run();//同步调用
+		System.out.println("run end...");
+	}
+	
+}
---
+++ thread/sync.java
@@ -0,0 +1,48 @@
+package thread;
+
+public class sync {
+    public static Object object = new Object();
+    public static void main(String[] args) {
+        Thread1 thread1 = new Thread1();
+        Thread2 thread2 = new Thread2();
+         
+        thread1.start();
+         
+//        try {
+//            Thread.sleep(200);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//         
+        thread2.start();
+    }
+     
+    static class Thread1 extends Thread{
+        @Override
+        public void run() {
+        	System.out.println("Thread1....");
+        	
+        	
+            synchronized (object) {
+               // System.out.println("线程"+Thread.currentThread().getName()+"调用了wait()");
+				//object.wait();//　调用某个对象的wait()方法，相当于让当前线程交出此对象的monitor，然后进入等待状态，等待后续再次获得此对象的锁
+            	
+            	object.notify();
+                System.out.println("1线程"+Thread.currentThread().getName()+"获取到了锁");
+            }
+        }
+    }
+     
+    static class Thread2 extends Thread{
+        @Override
+        public void run() {
+        	System.out.println("Thread2....");
+            synchronized (object) {
+                object.notify();//调用某个对象的notify()方法能够唤醒一个正在等待这个对象的monitor的线程
+                System.out.println("2线程"+Thread.currentThread().getName()+"调用了object.notify()");
+            }
+            //只有等调用完notify()或者notifyAll()并退出synchronized块，释放对象锁后,其余线程才可获得锁执行。
+            System.out.println("2线程"+Thread.currentThread().getName()+"释放了锁");
+        }
+    }
+}
