7c21afae011f80be5fa882b4a63fc1546dda50fc
yunkai <819664855@qq.com>
2020-05-26 14:35:32 +0800

    some container demo

--- thread_mulit/ThreadDemo.java
+++ thread_mulit/ThreadDemo.java
@@ -1,4 +1,8 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
+//继承Thread类实现现成类，在自己类中实现run方法
 class BThread extends Thread {
     public BThread() {
         super("[BThread] Thread");
@@ -35,6 +39,7 @@ class AThread extends Thread {
     }
 }
 
+//实现Runable接口，将类对象作为Thread的执行对象
 class MyRunable implements Runnable{
     @Override
     public void run() {
@@ -68,7 +73,7 @@ public class ThreadDemo {
 //    Timed Waiting：运行中的线程，因为执行sleep()方法正在计时等待；
 //    Terminated：线程已终止，因为run()方法执行完毕。
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         Thread t1 = new BThread();//启动1个线程，调用start方法 即可执行thread中的run方法
         t1.start();
 
@@ -90,6 +95,37 @@ public class ThreadDemo {
         });
         t4.run();
         System.out.println("main end");
+
+        //通过Callable和Future创建线程
+        class MyCallable implements Callable<String> {
+            String v = "";
+
+            public MyCallable(String v){
+                this.v = v;
+            }
+            @Override
+            public String call() throws Exception {
+                return this.v;
+            }
+        }
+
+        FutureTask<String> futureTask = new FutureTask<>(new MyCallable("zhangsan"));
+        FutureTask<String> futureTask2= new FutureTask<>(new MyCallable("lisi"));
+        /***
+         * futureTask 实现了 Runnable接口
+         * 所以新建线程的时候可以传入futureTask
+         * FutureTask重写的run方法中实际是调用了Callable接口在call()方法
+         * 所以执行线程的时候回执行call方法的内容
+         */
+        Thread thread = new Thread(futureTask);
+        thread.start();
+        String value = futureTask.get();
+        System.out.println(value);
+
+        Thread thread2 = new Thread(futureTask2);
+        thread2.start();
+        String value2 = futureTask2.get();
+        System.out.println(value2);
     }
 
 }
